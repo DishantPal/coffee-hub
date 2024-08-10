98a577f9f5ecc5b801d461be40df083b9bd1dd30
Dishant Pal <dspal@enacton.com>
2024-08-11 00:11:46 +0530

    added page cache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->use([
+            \Silber\PageCache\Middleware\CacheResponse::class,
+        ]);
+
+        $middleware->alias([
+            'page-cache' => \Silber\PageCache\Middleware\CacheResponse::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
